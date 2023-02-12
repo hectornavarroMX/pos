c9a06eb9c79f94917aac99f8171dc7ab7d7c7a26
Hector <hector@hector.com>
2023-02-11 20:07:29 -0600

    01video

---
+++ app/Views/footer.php
@@ -0,0 +1,23 @@
+
+<footer class="py-4 bg-light mt-auto">
+        <div class="container-fluid px-4">
+            <div class="d-flex align-items-center justify-content-between small">
+                <div class="text-muted">Héctor Navarro <?php echo date('Y'); ?></div>
+                <div>
+                    <a href="#" target="_blank">Facebook</a>
+                    &middot;
+                    <a href="#" target="_blank">YouTube</a>
+                </div>
+            </div>
+        </div>
+    </footer>
+</div>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+    crossorigin="anonymous"></script>
+<script src="js/scripts.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/simple-datatables@latest" crossorigin="anonymous"></script>
+<script src="js/datatables-simple-demo.js"></script>
+</body>
+
+</html>
